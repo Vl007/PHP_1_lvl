01afaa0cbb8e65f029311c001f4ceee48689fbf1
Vl007 <skorikov777@yandex.ru>
2020-06-18 14:25:06 +0300

    Дз 5...

--- Lesson 5/index.php
+++ Lesson 5/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles/style.css">
+    <script src="https://kit.fontawesome.com/2be8c6b35f.js" crossorigin="anonymous"></script>
     <title>Homework 5</title>
 </head>
 
@@ -13,7 +14,7 @@
 <div>
 
     <?php
-    require 'server.php';
+    require_once 'server.php';
     ?>
 
 </div>
